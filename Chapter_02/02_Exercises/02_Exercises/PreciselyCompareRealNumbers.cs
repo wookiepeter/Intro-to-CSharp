f5446d3ffcb23e221fe6f13ef5f56fd2b46d4497
wookiepeter <matthis.hagen@o2online.de>
2016-03-03 19:03:04 +0100

    started Project for Chapter_02 and continued exercises

---
+++ Chapter_02/02_Exercises/02_Exercises/PreciselyCompareRealNumbers.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_Exercises
+{
+    class PreciselyCompareRealNumbers
+    {
+        static void Main(string[] args)
+        {
+            double firstRealNumber;
+            double secondRealNumber;
+
+            Console.WriteLine("Please enter the first real number: ");
+            firstRealNumber = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please enter the second real number: ");
+            secondRealNumber = double.Parse(Console.ReadLine());
+
+            if (firstRealNumber == secondRealNumber)
+            {
+                Console.WriteLine("Both numbers are the same");
+            }
+            else
+            {
+                if (firstRealNumber > secondRealNumber)
+                {
+                    Console.WriteLine("first number > second number");
+                }
+                else
+                {
+                    Console.WriteLine("first number < second number");
+                }
+            }
+        }
+    }
+}
